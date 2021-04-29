72b449e2341490c37a5bec07e21d44d0b0a62fb7
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2021-04-29 15:34:22 +0200

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
+  has_many :created_events, foreign_key: :creator_id, class_name: "Event"
 end
