c30f999d79d834ed44806f907e213613307796d1
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2021-04-29 17:58:15 +0200

    updated show_event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
-
+    validates :title, :description, :date_time, :location, presence: true
     scope :past, -> { where('date_time < ?', Date.today).order(date_time: :desc) }
     scope :upcoming, -> { where('date_time > ?', Date.today).order(:date_time) }
 end
