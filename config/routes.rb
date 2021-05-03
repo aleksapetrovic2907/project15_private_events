3673668e32b95b00184f668cc083d8b84f69a842
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2021-05-03 11:42:46 +0200

    added rsvp routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  root "events#index"
+
   devise_for :users
   resources :users
-  resources :events
-  root "events#index"
+  resources :events do
+    member do
+      get 'rsvp'
+      get 'cancel_rsvp'
+    end
+  end
 end
