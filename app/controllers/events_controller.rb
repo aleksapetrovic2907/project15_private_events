2a21535da21415592e958c84c0d7dc7288e16d79
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2021-05-03 11:43:11 +0200

    attend event and cancel invite added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -33,6 +33,22 @@ class EventsController < ApplicationController
           render 'edit'
         end
     end
+
+    def rsvp
+      @event = Event.find(params[:id])
+      if @event.attendees.include?(current_user)
+        redirect_to @event, notice: "You are already on the list"
+      else
+        @event.attendees << current_user
+        redirect_to @event
+      end
+    end
+  
+    def cancel_rsvp
+      @event = Event.find(params[:id])
+      @event.attendees.delete(current_user)
+      redirect_to @event, notice: "You are no longer attending this event"
+    end
     
 
     def show
